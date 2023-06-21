833ebaf1f4b9180effc4cf9a2f0b0f82a710f806
James Emselle <james.e.0111.2@gmail.com>
2023-06-21 21:56:53 +1000

    Make test pass

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,12 @@
-const test = WasRun("testMethod");
+class WasRun {
+  wasRun: boolean | null = null;
+  constructor(name: string) {}
+  testMethod() {
+    this.wasRun = true;
+  }
+}
+
+const test = new WasRun("testMethod");
 console.log(test.wasRun);
 test.testMethod();
 console.log(test.wasRun);

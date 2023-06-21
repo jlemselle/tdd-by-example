40ae15daa3a43df1ece5d67a6e4a27e309b15df7
James Emselle <james.e.0111.2@gmail.com>
2023-06-21 22:47:50 +1000

    Finish off invoking test method

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,31 @@
-class WasRun {
-  wasRun: boolean | null = null;
-  constructor(private name: keyof WasRun) {}
-  testMethod() {
-    this.wasRun = true;
-  }
+import assert from "assert";
+
+class TestCase {
+  constructor(protected name: string) {}
+
   run() {
-    const method = this[this.name] as () => void;
+    const method = this[this.name as keyof TestCase] as () => void;
     method.call(this);
   }
 }
 
-const test = new WasRun("testMethod");
-console.log(test.wasRun);
-test.run();
-console.log(test.wasRun);
+class WasRun extends TestCase {
+  wasRun: boolean | null = null;
+  constructor(name: string) {
+    super(name);
+  }
+  testMethod() {
+    this.wasRun = true;
+  }
+}
+
+class TestCaseTest extends TestCase {
+  testRunning() {
+    const test = new WasRun("testMethod");
+    assert(!test.wasRun);
+    test.run();
+    assert(test.wasRun);
+  }
+}
+
+new TestCaseTest("testRunning").run();
